95e02a46554acb7dbc74c20ea76ec3204caca645
Sidsmartz <siddhartht4206@gmail.com>
2025-05-14 21:36:26 +0530

    Restart

--- README.md
+++
@@ -1,3 +0,0 @@
-A portfolio website with Nextjs, Tailwind CSS, GSAP, 3js, Spline and more!
-
-Currently Hosted at : https://sidsportfolio-psi.vercel.app
--- app/provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import * as react from "react"
-import { ThemeProviderProps } from "next-themes";
-import { ThemeProvider as NextThemeProvider } from "next-themes";
-
-export function ThemeProvider({ children , ...props } : ThemeProviderProps) {
-    return <NextThemeProvider {...props}>{children}</NextThemeProvider>
-}
